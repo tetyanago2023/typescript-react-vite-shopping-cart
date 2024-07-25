2a5a2dacba44fb7ccc6c268dab84874b9fd75ef2
Tetyana2015 <tgoncharenko2013@gmail.com>
2024-07-24 19:15:24 -0700

    useEffect in ProductProvider was implemented

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactElement, useState} from "react";
+import {createContext, ReactElement, useEffect, useState} from "react";
 
 export type ProductsType = {
     sku: string,
@@ -6,23 +6,25 @@ export type ProductsType = {
     price: number,
 }
 
-const initialState: ProductsType[] = [
-    {
-        "sku": "item0001",
-        "name": "Widget",
-        "price": 9.99
-    },
-    {
-        "sku": "item0002",
-        "name": "Premium Widget",
-        "price": 19.99
-    },
-    {
-        "sku": "item0003",
-        "name": "Deluxe Widget",
-        "price": 29.99
-    }
-]
+const initialState: ProductsType[] = []
+
+// const initialState: ProductsType[] = [
+//     {
+//         "sku": "item0001",
+//         "name": "Widget",
+//         "price": 9.99
+//     },
+//     {
+//         "sku": "item0002",
+//         "name": "Premium Widget",
+//         "price": 19.99
+//     },
+//     {
+//         "sku": "item0003",
+//         "name": "Deluxe Widget",
+//         "price": 29.99
+//     }
+// ]
 
 export type UseProductsContextType = {
     products: ProductsType[]
@@ -40,6 +42,25 @@ type ChildrenType = {
 
 export const ProductsProvider = ({children}: ChildrenType): ReactElement => {
     const[products, setProducts] = useState<ProductsType[]>(initialState)
+
+    useEffect(() => {
+        const fetchProducts = async (): Promise<ProductsType[]> => {
+            const data =
+                await fetch('http://localhost:3500/products')
+                    .then(res => {
+                        return res.json()
+                    })
+                    .catch(err => {
+                        if (err instanceof Error) {
+                            console.error(err.message)
+                        }
+                    })
+            return data
+        }
+
+        fetchProducts().then(products => setProducts(products))
+    }, [])
+
     return (
         // <ProductsContext.Provider value={{ products: initialState }}>
         <ProductsContext.Provider value={{ products }}>
